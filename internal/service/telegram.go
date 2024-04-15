7fcb87e26ad27426ebd269dbd1742d6840829c20
ilyadubrovsky <kordiffo@gmail.com>
2024-04-15 21:32:25 +0300

    [v2.0.0] realized telegram service logic

--- internal/service/telegram.go
+++ internal/service/telegram.go
@@ -1,4 +1,7 @@
 package service
 
+// TODO тут нужно будет завязаться на опции сервиса, а не телебота
 type Telegram interface {
+	SendMessageWithOpts(id int64, message string, opts ...interface{}) error
+	EditMessageWithOpts(id int64, messageID int, msg string, opts ...interface{}) error
 }
