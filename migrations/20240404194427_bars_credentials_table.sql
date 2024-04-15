87b260c715aec3817a866f83066f6eec270f8f39
ilyadubrovsky <kordiffo@gmail.com>
2024-04-15 21:32:25 +0300

    [v2.0.0] updated bars credentials migration

--- migrations/20240404194427_bars_credentials_table.sql
+++ migrations/20240404194427_bars_credentials_table.sql
@@ -1,8 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE bars_credentials (
-    id UUID PRIMARY KEY,
-    user_id BIGINT NOT NULL REFERENCES users (id),
+    user_id BIGINT PRIMARY KEY REFERENCES users (id),
     username TEXT NOT NULL,
     password BYTEA NOT NULL,
     created_at TIMESTAMPTZ NOT NULL,
