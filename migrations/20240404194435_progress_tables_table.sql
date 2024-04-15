3a1bd160ea86fadae59b0ecf0607c3f7db4bb320
ilyadubrovsky <kordiffo@gmail.com>
2024-04-15 21:32:30 +0300

    [v2.0.0] updated progress tables table migration

--- migrations/20240404194435_progress_tables_table.sql
+++ migrations/20240404194435_progress_tables_table.sql
@@ -1,11 +1,10 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE progress_tables (
-    id UUID PRIMARY KEY,
-    user_id BIGINT NOT NULL REFERENCES bars_credentials (user_id),
+    user_id BIGINT PRIMARY KEY REFERENCES bars_credentials (user_id),
     progress_table JSONB NOT NULL,
     created_at TIMESTAMPTZ NOT NULL,
-    updated_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL
 );
 -- +goose StatementEnd
 
