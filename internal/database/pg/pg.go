368a28e2b817abbbb6daa3dec6cb7c529d08e528
ilyadubrovsky <kordiffo@gmail.com>
2024-04-16 14:34:12 +0300

    [v2.0.2] fixed pg connection

--- internal/database/pg/pg.go
+++ internal/database/pg/pg.go
@@ -5,13 +5,13 @@ import (
 	"fmt"
 
 	"github.com/ilyadubrovsky/tracking-bars/internal/database"
-	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 func New(ctx context.Context, dsn string) (database.PG, error) {
-	conn, err := pgx.Connect(ctx, dsn)
+	conn, err := pgxpool.Connect(ctx, dsn)
 	if err != nil {
-		return nil, fmt.Errorf("pgx.Connect: %w", err)
+		return nil, fmt.Errorf("pgxpool.Connect: %w", err)
 	}
 
 	if err = conn.Ping(ctx); err != nil {
