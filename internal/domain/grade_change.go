05609aa11739a95069a485cb0593595274fd1ff1
ilyadubrovsky <kordiffo@gmail.com>
2024-06-06 23:31:39 +0300

    [v2.4.0] fixed grade change text

--- internal/domain/grade_change.go
+++ internal/domain/grade_change.go
@@ -12,6 +12,6 @@ type GradeChange struct {
 
 // TODO это явно не логика для домеина, нужно переделать
 func (c *GradeChange) String() string {
-	return fmt.Sprintf("*Получение изменение:*\n\n*Название дисциплины:*\n%s\n\n*Контрольное мероприятие:*\n%s\n\n*Старая оценка:*\n%s\n\n"+
+	return fmt.Sprintf("*Получено изменение:*\n\n*Название дисциплины:*\n%s\n\n*Контрольное мероприятие:*\n%s\n\n*Старая оценка:*\n%s\n\n"+
 		"*Новая оценка:*\n%s", c.Discipline, c.ControlEvent, c.OldGrade, c.NewGrade)
 }
